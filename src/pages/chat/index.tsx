0ff639c61f8c3d02d98dd03b686a7caba6d66395
rain <15911122312@163.com>
2025-03-11 14:43:40 +0800

    fix: add history reload for coco chat

--- src/pages/chat/index.tsx
+++ src/pages/chat/index.tsx
@@ -149,6 +149,7 @@ export default function Chat({}: ChatProps) {
               }}
               onSelectChat={onSelectChat}
               onDeleteChat={deleteChat}
+              fetchChatHistory={getChatHistory}
             />
           </div>
         ) : null}
