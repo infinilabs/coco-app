0b57f840d08478317e8745d546bb38a684e213e1
ayang <473033518@qq.com>
2025-03-31 20:33:25 +0800

    feat: new chat added to suggestion list

---
+++ src/components/ChatMessage/PrevSuggestion.tsx
@@ -0,0 +1,56 @@
+import { Get } from "@/api/axiosRequest";
+import { MoveRight } from "lucide-react";
+import { FC, useEffect, useState } from "react";
+
+interface PrevSuggestionProps {
+  id: string;
+}
+
+const PrevSuggestion: FC<PrevSuggestionProps> = (props) => {
+  const { id } = props;
+
+  const [list, setList] = useState<string[]>([]);
+
+  useEffect(() => {
+    getList();
+  }, [id]);
+
+  const getList = async () => {
+    if (!id) return;
+
+    const url = `/integration/${id}/chat/_suggest`;
+
+    const [error, res] = await Get(`/integration/${id}/chat/_suggest`);
+
+    if (error) {
+      console.error(url, error);
+
+      return setList([]);
+    }
+
+    console.log("res", res);
+
+    setList(res as any);
+  };
+
+  console.log("id", id);
+
+  return (
+    <ul className="absolute left-2 bottom-2 flex flex-col gap-2">
+      {list.map((item) => {
+        return (
+          <li
+            key={item}
+            className="flex items-center self-start gap-2 px-3 py-2 leading-4 text-sm text-[#333] dark:text-[#d8d8d8] rounded-xl border border-black/15 dark:border-white/15 hover:!border-[#0072ff] hover:!text-[#0072ff] transition cursor-pointer"
+          >
+            {item}
+
+            <MoveRight className="size-4" />
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+
+export default PrevSuggestion;
