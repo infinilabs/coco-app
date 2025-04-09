5bb9d53be2ec8745c90792cbfa3e2ee847d8b7b9
ayang <473033518@qq.com>
2025-04-09 16:26:52 +0800

    refactor: remove unused code

--- src-tauri/src/setup/windows.rs
+++ src-tauri/src/setup/windows.rs
@@ -1,5 +1,3 @@
 use tauri::{App, WebviewWindow};
 
-pub fn platform(_app: &mut App, main_window: WebviewWindow, _settings_window: WebviewWindow) {
-    let _ = main_window.set_shadow(false);
-}
+pub fn platform(_app: &mut App, _main_window: WebviewWindow, _settings_window: WebviewWindow) {}
