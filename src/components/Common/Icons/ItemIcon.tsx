f18f94ea6de8574834c4bfd14e4685990a850205
ayangweb <75017711+ayangweb@users.noreply.github.com>
2025-04-18 10:18:28 +0800

    refactor: replace the app icon after search with an absolute path (#387)

--- src/components/Common/Icons/ItemIcon.tsx
+++ src/components/Common/Icons/ItemIcon.tsx
@@ -1,11 +1,14 @@
 import { File } from "lucide-react";
-import React from 'react';
+import React, { useState } from "react";
+import { useMount } from "ahooks";
 
 import IconWrapper from "./IconWrapper";
 import ThemedIcon from "./ThemedIcon";
 import { useFindConnectorIcon } from "@/hooks/useFindConnectorIcon";
 import { useAppStore } from "@/stores/appStore";
 import FontIcon from "./FontIcon";
+import platformAdapter from "@/utils/platformAdapter";
+import { isNil } from "lodash-es";
 
 interface ItemIconProps {
   item: any;
@@ -19,60 +22,87 @@ const ItemIcon = React.memo(function ItemIcon({
   onClick = () => {},
 }: ItemIconProps) {
   const endpoint_http = useAppStore((state) => state.endpoint_http);
-
   const connectorSource = useFindConnectorIcon(item);
-  // console.log("connectorSource", connectorSource);
   const icons = connectorSource?.assets?.icons || {};
+  const [isAbsolute, setIsAbsolute] = useState<boolean>();
 
-  if (item?.icon?.startsWith("font_")) {
-    return (
-      <FontIcon name={item?.icon} className={className} />
-    );
-  }
+  useMount(async () => {
+    if (!item?.icon) {
+      return setIsAbsolute(false);
+    }
 
-  // If the icon is a valid base64-encoded image
-  const isBase64 = item?.icon?.startsWith("data:image/");
-  if (isBase64) {
-    return (
-      <IconWrapper className={className} onClick={onClick}>
-        <img className={className} src={item?.icon} alt="icon" />
-      </IconWrapper>
-    );
-  }
+    const { isAbsolute } = await platformAdapter.metadata(item.icon, {
+      omitSize: true,
+    });
 
-  let selectedIcon = icons[item?.icon];
-  if (!selectedIcon) {
-    selectedIcon = item?.icon;
-  }
+    setIsAbsolute(isAbsolute);
+  });
 
-  if (!selectedIcon) {
-    return (
-      <IconWrapper className={className} onClick={onClick}>
-        <ThemedIcon component={File} className={className} />
-      </IconWrapper>
-    );
-  }
+  const renderIcon = () => {
+    if (isNil(isAbsolute)) return;
 
-  if (
-    selectedIcon.startsWith("http://") ||
-    selectedIcon.startsWith("https://")
-  ) {
-    return (
-      <IconWrapper className={className} onClick={onClick}>
-        <img className={className} src={selectedIcon} alt="icon" />
-      </IconWrapper>
-    );
-  } else {
-    return (
-      <IconWrapper className={className} onClick={onClick}>
-        <img
-          className={className}
-          src={`${endpoint_http}${selectedIcon}`}
-          alt="icon"
-        />
-      </IconWrapper>
-    );
-  }
-})
+    if (isAbsolute) {
+      return (
+        <IconWrapper className={className} onClick={onClick}>
+          <img
+            className={className}
+            src={platformAdapter.convertFileSrc(item?.icon)}
+            alt="icon"
+          />
+        </IconWrapper>
+      );
+    } else {
+      if (item?.icon?.startsWith("font_")) {
+        return <FontIcon name={item?.icon} className={className} />;
+      }
+
+      // If the icon is a valid base64-encoded image
+      const isBase64 = item?.icon?.startsWith("data:image/");
+      if (isBase64) {
+        return (
+          <IconWrapper className={className} onClick={onClick}>
+            <img className={className} src={item?.icon} alt="icon" />
+          </IconWrapper>
+        );
+      }
+
+      let selectedIcon = icons[item?.icon];
+      if (!selectedIcon) {
+        selectedIcon = item?.icon;
+      }
+
+      if (!selectedIcon) {
+        return (
+          <IconWrapper className={className} onClick={onClick}>
+            <ThemedIcon component={File} className={className} />
+          </IconWrapper>
+        );
+      }
+
+      if (
+        selectedIcon.startsWith("http://") ||
+        selectedIcon.startsWith("https://")
+      ) {
+        return (
+          <IconWrapper className={className} onClick={onClick}>
+            <img className={className} src={selectedIcon} alt="icon" />
+          </IconWrapper>
+        );
+      } else {
+        return (
+          <IconWrapper className={className} onClick={onClick}>
+            <img
+              className={className}
+              src={`${endpoint_http}${selectedIcon}`}
+              alt="icon"
+            />
+          </IconWrapper>
+        );
+      }
+    }
+  };
+
+  return renderIcon();
+});
 
 export default ItemIcon;
