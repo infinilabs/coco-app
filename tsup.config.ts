e60915443adc36c66efb148926e353e72b2557a8
BiggerRain <15911122312@163.COM>
2025-04-27 22:19:38 +0800

    build: build web 1.1.14 (#456)

--- tsup.config.ts
+++ tsup.config.ts
@@ -67,7 +67,7 @@ export default defineConfig({
 
     const packageJson = {
       name: "@infinilabs/search-chat",
-      version: "1.1.12",
+      version: "1.1.14",
       main: "index.js",
       module: "index.js",
       type: "module",
