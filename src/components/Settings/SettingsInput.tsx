c5f8c73efe869b3651d082296f20f7c48c3158f0
ayang <473033518@qq.com>
2025-05-12 14:53:00 +0800

    refactor: refactor the content style of the extended page

--- src/components/Settings/SettingsInput.tsx
+++ src/components/Settings/SettingsInput.tsx
@@ -9,7 +9,7 @@ const SettingsInput: FC<InputProps> = (props) => {
     <Input
       {...rest}
       className={clsx(
-        "w-20 h-8 px-2 rounded-md border bg-transparent border-black/5 dark:border-white/10 hover:border-[#0072FF] focus:border-[#0072FF] transition",
+        "w-20 h-8 px-2 rounded-md border bg-transparent border-black/5 dark:border-white/10 hover:!border-[#0072FF] focus:!border-[#0072FF] transition",
         className
       )}
     />
--- src/stores/extension.ts
+++ src/stores/extension.ts
@@ -1,24 +1,13 @@
 import { create } from "zustand";
 import { persist, subscribeWithSelector } from "zustand/middleware";
 
-export type IExtensionStore = {
-  disabledExtensions: string[];
-  setDisabledExtensions: (disabledExtensions: string[]) => void;
-};
+export type IExtensionStore = {};
 
 export const useExtensionStore = create<IExtensionStore>()(
   subscribeWithSelector(
-    persist(
-      (set) => ({
-        disabledExtensions: [],
-        setDisabledExtensions: (disabledExtensions) => {
-          return set({ disabledExtensions });
-        },
-      }),
-      {
-        name: "extension-store",
-        partialize: () => ({}),
-      }
-    )
+    persist(() => ({}), {
+      name: "extension-store",
+      partialize: () => ({}),
+    })
   )
 );
