614301e17b070a001e75a8686967de5f3e945eed
rain <15911122312@163.com>
2025-05-13 10:56:51 +0800

    chore: greetings show hidden logic

--- src/components/Assistant/ChatContent.tsx
+++ src/components/Assistant/ChatContent.tsx
@@ -102,7 +102,7 @@ export const ChatContent = ({
         className="flex-1 w-full overflow-x-hidden overflow-y-auto border-t border-[rgba(0,0,0,0.1)] dark:border-[rgba(255,255,255,0.15)] custom-scrollbar relative"
         onScroll={handleScroll}
       >
-        {activeChat?.messages?.length === 0 && <Greetings />}
+        {(!activeChat || activeChat?.messages?.length === 0) && <Greetings />}
 
         {activeChat?.messages?.map((message, index) => (
           <ChatMessage
