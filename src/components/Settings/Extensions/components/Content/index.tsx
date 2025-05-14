9f97f5c101f12b73e18a59e5981745d8b6323acf
ayang <473033518@qq.com>
2025-05-14 11:39:41 +0800

    refactor: changing the timing of app list loading

--- src/components/Settings/Extensions/components/Content/index.tsx
+++ src/components/Settings/Extensions/components/Content/index.tsx
@@ -1,6 +1,13 @@
-import { cloneElement, Fragment, MouseEvent, useContext } from "react";
+import {
+  cloneElement,
+  FC,
+  Fragment,
+  MouseEvent,
+  useContext,
+  useState,
+} from "react";
 import { ExtensionsContext, Plugin } from "../..";
-import { useReactive } from "ahooks";
+import { useMount } from "ahooks";
 import { ChevronRight, LoaderCircle } from "lucide-react";
 import clsx from "clsx";
 import { isArray, isFunction } from "lodash-es";
@@ -10,30 +17,60 @@ import Shortcut from "../Shortcut";
 import SettingsInput from "@/components/Settings/SettingsInput";
 import { useTranslation } from "react-i18next";
 
-interface State {
-  expands: string[];
-}
-
 const Content = () => {
-  const { plugins, activeId, setPlugins, setActiveId } =
-    useContext(ExtensionsContext);
-  const state = useReactive<State>({
-    expands: [],
+  const { plugins } = useContext(ExtensionsContext);
+
+  return plugins.map((item) => {
+    return <Item key={item.id} {...item} level={1} />;
   });
+};
+
+const Item: FC<Plugin & { level: number }> = (props) => {
+  const {
+    id,
+    icon,
+    name,
+    children,
+    type = "Extension",
+    manualLoad,
+    level = 1,
+  } = props;
+  const { activeId, setActiveId, setPlugins } = useContext(ExtensionsContext);
+  const [loading, setLoading] = useState(false);
+  const [expanded, setExpanded] = useState(false);
   const { t } = useTranslation();
+  const hasChildren = isArray(children);
 
-  const handleExpand = (event: MouseEvent, id: string) => {
-    event.stopPropagation();
+  const handleLoadChildren = async () => {
+    setLoading(true);
 
-    if (state.expands.includes(id)) {
-      state.expands = state.expands.filter((item) => item !== id);
+    await props.loadChildren?.();
+
+    setLoading(false);
+  };
+
+  useMount(async () => {
+    if (!manualLoad) {
+      handleLoadChildren();
+    }
+  });
+
+  const handleExpand = async (event: MouseEvent) => {
+    event?.stopPropagation();
+
+    if (expanded) {
+      setExpanded(false);
     } else {
-      state.expands.push(id);
+      if (manualLoad) {
+        await handleLoadChildren();
+      }
+
+      setExpanded(true);
     }
   };
 
-  const renderAlias = (plugin: Plugin) => {
-    const { alias, onAliasChange } = plugin;
+  const renderAlias = () => {
+    const { alias, onAliasChange } = props;
 
     const handleChange = (value: string) => {
       if (isFunction(onAliasChange)) {
@@ -64,8 +101,8 @@ const Content = () => {
     return <>--</>;
   };
 
-  const renderHotkey = (plugin: Plugin) => {
-    const { hotkey, onHotkeyChange } = plugin;
+  const renderHotkey = () => {
+    const { hotkey, onHotkeyChange } = props;
 
     const handleChange = (value: string) => {
       if (isFunction(onHotkeyChange)) {
@@ -93,8 +130,8 @@ const Content = () => {
     return <>--</>;
   };
 
-  const renderSwitch = (plugin: Plugin) => {
-    const { id, enabled = true, onEnabledChange } = plugin;
+  const renderSwitch = () => {
+    const { enabled = true, onEnabledChange } = props;
 
     const handleChange = (value: boolean) => {
       if (isFunction(onEnabledChange)) {
@@ -134,82 +171,71 @@ const Content = () => {
     );
   };
 
-  const renderContent = (data = plugins, level = 1) => {
-    return data.map((plugin) => {
-      const { id, icon, name, children, type = "Extension" } = plugin;
-
-      const hasChildren = isArray(children);
-      const expanded = state.expands.includes(id);
-
-      return (
-        <Fragment key={id}>
+  return (
+    <Fragment key={id}>
+      <div
+        className={clsx("-mx-2 px-2 text-sm rounded-md", {
+          "bg-[#f0f6fe] dark:bg-gray-700": id === activeId,
+        })}
+      >
+        <div
+          className="flex items-center justify-between gap-2 h-8"
+          onClick={() => {
+            setActiveId(id);
+          }}
+        >
           <div
-            className={clsx("-mx-2 px-2 text-sm rounded-md", {
-              "bg-[#f0f6fe] dark:bg-gray-700": id === activeId,
-            })}
+            className="flex-1 flex items-center gap-1 overflow-hidden"
+            style={{ paddingLeft: (level - 1) * 20 }}
           >
-            <div
-              className="flex items-center justify-between gap-2 h-8"
-              onClick={() => {
-                setActiveId(id);
-              }}
-            >
-              <div
-                className="flex-1 flex items-center gap-1 overflow-hidden"
-                style={{ paddingLeft: (level - 1) * 20 }}
-              >
-                <div className="min-w-4 h-4">
-                  {hasChildren && (
-                    <>
-                      {children.length === 0 ? (
-                        <LoaderCircle className="size-4 animate-spin" />
-                      ) : (
-                        <ChevronRight
-                          onClick={(event) => {
-                            handleExpand(event, id);
-                          }}
-                          className={clsx("size-4 transition cursor-pointer", {
-                            "rotate-90": expanded,
-                          })}
-                        />
-                      )}
-                    </>
+            <div className="min-w-4 h-4">
+              {hasChildren && (
+                <>
+                  {loading ? (
+                    <LoaderCircle className="size-4 animate-spin" />
+                  ) : (
+                    <ChevronRight
+                      onClick={handleExpand}
+                      className={clsx("size-4 transition cursor-pointer", {
+                        "rotate-90": expanded,
+                      })}
+                    />
                   )}
-                </div>
-
-                {cloneElement(icon, {
-                  className: clsx("size-4", icon.props.className),
-                })}
-
-                <div className="truncate">{name}</div>
-              </div>
-
-              <div className="w-3/5 flex items-center text-[#999]">
-                <div className="flex-1">{type}</div>
-                <div className="flex-1">{renderAlias(plugin)}</div>
-                <div className="flex-1">{renderHotkey(plugin)}</div>
-                <div className="flex-1 flex items-center justify-end">
-                  {renderSwitch(plugin)}
-                </div>
-              </div>
+                </>
+              )}
             </div>
+
+            {cloneElement(icon, {
+              className: clsx("size-4", icon.props.className),
+            })}
+
+            <div className="truncate">{name}</div>
           </div>
 
-          {hasChildren && (
-            <div
-              className={clsx({
-                hidden: !expanded,
-              })}
-            >
-              {renderContent(children, level + 1)}
+          <div className="w-3/5 flex items-center text-[#999]">
+            <div className="flex-1">{type}</div>
+            <div className="flex-1">{renderAlias()}</div>
+            <div className="flex-1">{renderHotkey()}</div>
+            <div className="flex-1 flex items-center justify-end">
+              {renderSwitch()}
             </div>
-          )}
-        </Fragment>
-      );
-    });
-  };
+          </div>
+        </div>
+      </div>
 
-  return renderContent();
+      {hasChildren && (
+        <div
+          className={clsx({
+            hidden: !expanded,
+          })}
+        >
+          {children.map((item) => {
+            return <Item key={item.id} {...item} level={level + 1} />;
+          })}
+        </div>
+      )}
+    </Fragment>
+  );
 };
 
 export default Content;
