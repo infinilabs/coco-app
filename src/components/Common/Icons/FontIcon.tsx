f690dbaab26c2c91e58610cfd4a93717057e2025
ayangweb <75017711+ayangweb@users.noreply.github.com>
2025-06-04 11:30:59 +0800

    refactor: web use the default icon for now (#602)

--- src/components/Common/Icons/FontIcon.tsx
+++ src/components/Common/Icons/FontIcon.tsx
@@ -1,15 +1,25 @@
+import { useAppStore } from "@/stores/appStore";
+import logoImg from "@/assets/icon.svg";
+import { FC } from "react";
+
 interface FontIconProps {
   name: string;
   className?: string;
   style?: React.CSSProperties;
 }
 
-const FontIcon = ({ name, className, style, ...rest }: FontIconProps) => {
-  return (
-    <svg className={`icon ${className || ""}`} style={style} {...rest}>
-      <use xlinkHref={`#${name}`} />
-    </svg>
-  );
+const FontIcon: FC<FontIconProps> = ({ name, className, style, ...rest }) => {
+  const isTauri = useAppStore((state) => state.isTauri);
+
+  if (isTauri) {
+    return (
+      <svg className={`icon ${className || ""}`} style={style} {...rest}>
+        <use xlinkHref={`#${name}`} />
+      </svg>
+    );
+  } else {
+    return <img src={logoImg} className={className} alt={"coco"} />;
+  }
 };
 
 export default FontIcon;
