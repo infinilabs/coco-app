18c5f33cd7f54d6f885e7e6f937868d9153617bc
ayang <473033518@qq.com>
2025-06-11 16:19:30 +0800

    refactor: re-enable the service to get a list of assistants

--- src/components/Assistant/AssistantList.tsx
+++ src/components/Assistant/AssistantList.tsx
@@ -17,6 +17,7 @@ import { AssistantFetcher } from "./AssistantFetcher";
 import AssistantItem from "./AssistantItem";
 import Pagination from "@/components/Common/Pagination";
 import { useSearchStore } from "@/stores/searchStore";
+import { useChatStore } from "@/stores/chatStore";
 
 interface AssistantListProps {
   assistantIDs?: string[];
@@ -43,15 +44,34 @@ export function AssistantList({ assistantIDs = [] }: AssistantListProps) {
     return state.setAskAiAssistantId;
   });
   const assistantList = useConnectStore((state) => state.assistantList);
+  const connected = useChatStore((state) => {
+    return state.connected;
+  });
 
   const { fetchAssistant } = AssistantFetcher({
     debounceKeyword,
     assistantIDs,
   });
 
-  const { pagination, runAsync } = usePagination(fetchAssistant, {
+  const getAssistants = (params: { current: number; pageSize: number }) => {
+    if (!connected) {
+      return Promise.resolve({
+        total: 0,
+        list: [],
+      });
+    }
+
+    return fetchAssistant(params);
+  };
+
+  const { pagination, runAsync } = usePagination(getAssistants, {
     defaultPageSize: 5,
-    refreshDeps: [currentService?.id, debounceKeyword, currentService?.enabled],
+    refreshDeps: [
+      currentService?.id,
+      debounceKeyword,
+      currentService?.enabled,
+      connected,
+    ],
     onSuccess(data) {
       setAssistants(data.list);
 
