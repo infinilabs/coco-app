500aba3100ef2b9412ddc0e473f051ba2b463a04
ayang <473033518@qq.com>
2025-06-25 18:10:56 +0800

    update

--- src/components/Settings/Extensions/index.tsx
+++ src/components/Settings/Extensions/index.tsx
@@ -141,7 +141,7 @@ export const Extensions = () => {
               {t("settings.extensions.title")}
             </h2>
 
-            <Button className="flex items-center justify-center size-6 border rounded-md hover:border-[#0096FB] transition">
+            <Button className="flex items-center justify-center size-6 border rounded-md dark:border-gray-700 hover:!border-[#0096FB] transition">
               <Plus className="size-4 text-[#0096FB]" />
             </Button>
           </div>
