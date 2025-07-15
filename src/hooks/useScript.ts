0584404011ece40bc70664979c88b75b0c63fd74
ayang <473033518@qq.com>
2025-07-15 09:08:08 +0800

    refactor: update the font icon link

--- src/hooks/useScript.ts
+++ src/hooks/useScript.ts
@@ -30,5 +30,5 @@ export const useIconfontScript = () => {
   // Coco Server Icons
   useScript("https://at.alicdn.com/t/c/font_4878526_cykw3et0ezd.js");
   // Coco App Icons
-  useScript("https://at.alicdn.com/t/c/font_4934333_yvzl3o3jgk.js");
+  useScript("https://at.alicdn.com/t/c/font_4934333_zclkkzo4fgo.js");
 };
