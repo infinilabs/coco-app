f2b4dab4b3b6879b3a9177fad5defde89a97af5a
rain9 <15911122312@163.com>
2025-07-31 12:02:28 +0800

    focs: update notes

--- docs/content.en/docs/release-notes/_index.md
+++ docs/content.en/docs/release-notes/_index.md
@@ -22,6 +22,7 @@ Information about release notes of Coco App is provided here.
 
 ### ✈️ Improvements
 
+- refactor: calling service related interfaces #831
 - refactor: split query_coco_fusion() #836
 - chore: web component loading font icon #838
 - chore: delete unused code files and dependencies #841
--- src/pages/settings/index.tsx
+++ src/pages/settings/index.tsx
@@ -39,7 +39,7 @@ function SettingsPage() {
     { name: t("settings.tabs.about"), icon: Info },
   ];
 
-  const [defaultIndex, setDefaultIndex] = useState<number>(2);
+  const [defaultIndex, setDefaultIndex] = useState<number>(0);
 
   useEffect(() => {
     const unlisten = listen("tab_index", (event) => {
