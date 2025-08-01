568db6aba04ce22357be378f7689be611a67ea7c
ayangweb <75017711+ayangweb@users.noreply.github.com>
2025-08-01 18:28:37 +0800

    feat: add extension uninstall option in settings (#855)
    * feat: add extension uninstall option in settings
    
    * docs: update changelog
    
    * update

--- src/components/Settings/Extensions/components/Details/index.tsx
+++ src/components/Settings/Extensions/components/Details/index.tsx
@@ -8,6 +8,11 @@ import SharedAi from "./SharedAi";
 import AiOverview from "./AiOverview";
 import Calculator from "./Calculator";
 import FileSearch from "./FileSearch";
+import { Ellipsis } from "lucide-react";
+import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/react";
+import platformAdapter from "@/utils/platformAdapter";
+import { useAppStore } from "@/stores/appStore";
+import { useTranslation } from "react-i18next";
 
 const Details = () => {
   const { rootState } = useContext(ExtensionsContext);
@@ -23,6 +28,10 @@ const Details = () => {
   const setQuickAiAccessAssistant = useExtensionsStore((state) => {
     return state.setQuickAiAccessAssistant;
   });
+  const addError = useAppStore((state) => {
+    return state.addError;
+  });
+  const { t } = useTranslation();
 
   const renderContent = () => {
     if (!rootState.activeExtension) return;
@@ -66,12 +75,62 @@ const Details = () => {
   };
 
   return (
-    <div className="flex-1 h-full overflow-auto">
-      <h2 className="text-lg font-semibold text-gray-900 dark:text-white mb-4">
-        {rootState.activeExtension?.name}
-      </h2>
+    <div className="flex-1 h-full pr-4 pb-4 overflow-auto">
+      <div className="flex items-start justify-between gap-4 mb-4">
+        <h2 className="text-lg font-semibold text-gray-900 dark:text-white">
+          {rootState.activeExtension?.name}
+        </h2>
 
-      <div className="pr-4 pb-4 text-sm">{renderContent()}</div>
+        {rootState.activeExtension?.developer && (
+          <Menu>
+            <MenuButton className="h-7">
+              <Ellipsis className="size-5" />
+            </MenuButton>
+
+            <MenuItems
+              anchor="bottom end"
+              className="p-1 text-sm bg-white dark:bg-[#202126] rounded-lg shadow-xs border border-gray-200 dark:border-gray-700"
+            >
+              <MenuItem>
+                <div
+                  className="px-3 py-2 text-nowrap text-red-500 hover:bg-black/5 hover:dark:bg-white/5 rounded-lg cursor-pointer"
+                  onClick={async () => {
+                    try {
+                      const { id, developer } = rootState.activeExtension!;
+
+                      await platformAdapter.invokeBackend(
+                        "uninstall_extension",
+                        {
+                          extensionId: id,
+                          developer: developer,
+                        }
+                      );
+
+                      Object.assign(rootState, {
+                        activeExtension: void 0,
+                        extensions: rootState.extensions.filter((item) => {
+                          return item.id !== id;
+                        }),
+                      });
+
+                      addError(
+                        t("settings.extensions.hints.uninstallSuccess"),
+                        "info"
+                      );
+                    } catch (error) {
+                      addError(String(error));
+                    }
+                  }}
+                >
+                  {t("settings.extensions.hints.uninstall")}
+                </div>
+              </MenuItem>
+            </MenuItems>
+          </Menu>
+        )}
+      </div>
+
+      <div className="text-sm">{renderContent()}</div>
     </div>
   );
 };
