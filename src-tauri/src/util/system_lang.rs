ee2df418d3041ff4a69467ae48854d2d05b67e01
Steve Lau <stevelauc@outlook.com>
2025-08-26 16:24:57 +0800

    chore: clean up unused warning
    The function `get_system_lang()` will only be used when the feature
    "use_pizza_engine" is enabled, feature-gate it to clear the compiler
    warning.

--- src-tauri/src/util/system_lang.rs
+++ src-tauri/src/util/system_lang.rs
@@ -1,10 +1,11 @@
-use sys_locale::get_locale;
-
 /// Helper function to get the system language.
 ///
 /// We cannot return `enum Lang` here because Coco has limited language support
 /// but the OS supports many more languages.
+#[cfg(feature = "use_pizza_engine")]
 pub(crate) fn get_system_lang() -> String {
+    use sys_locale::get_locale;
+
     // fall back to English (general) when we cannot get the locale
     //
     // We replace '-' with '_' in applications-rs, to make the locales match,
