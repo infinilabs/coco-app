17f6c0c972dcc8a4000e2a516411ebe7034bb008
ayang <473033518@qq.com>
2025-09-26 11:50:40 +0800

    refactor: update i18n

--- src/components/Search/ContextMenu.tsx
+++ src/components/Search/ContextMenu.tsx
@@ -178,7 +178,7 @@ const ContextMenu = ({ formatUrl }: ContextMenuProps) => {
         },
       },
       {
-        name: t("search.contextMenu.openContainingFolder"),
+        name: t("search.contextMenu.openFileLocation"),
         icon: <SquareArrowOutUpRight />,
         keys: isMac ? ["⌘", "↩︎"] : ["Ctrl", "Enter"],
         shortcut: isMac ? "meta.enter" : "ctrl.enter",
