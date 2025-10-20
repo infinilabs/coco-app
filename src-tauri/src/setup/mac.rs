a68c9752faae56a58e02553beaaa1b2a3b976892
Steve Lau <stevelauc@outlook.com>
2025-10-20 15:16:06 +0800

    refactor: bump tauri_nspanel & show_coco/hide_coco now use NSPanel's function on macOS
    This commit:
    
    1. Bump dep tauri_nspenel to v2.1
    2. On macOS, our main window is not a window but panel. Previously, we
       were using window.show() and window.hide() in show_coco() and
       hide_coco(). In this commit, we switch from window.show/hide to
       panel.show/hide
    
    Co-authored-by: ayang <473033518@qq.com>

--- src-tauri/src/setup/mac.rs
+++ src-tauri/src/setup/mac.rs
@@ -1,23 +1,26 @@
 //! credits to: https://github.com/ayangweb/ayangweb-EcoPaste/blob/169323dbe6365ffe4abb64d867439ed2ea84c6d1/src-tauri/src/core/setup/mac.rs
-//!
-//! # allow(deprecated)
-//!
-//! This file uses some deprecated interfaces from the `tauri_nspanel` crate. The
-//! only way to get rid of them is to bump that crate (v2->v2.1), we are not going
-//! to do that because doing that bump requires a re-write of the code in this
-//! file and v2 has been working well. So we allow these deprecated interfaces.
 
 use crate::common::MAIN_WINDOW_LABEL;
-use objc2_app_kit::NSNonactivatingPanelMask;
-use tauri::{AppHandle, Emitter, EventTarget, WebviewWindow};
-#[allow(deprecated)]
-use tauri_nspanel::cocoa::appkit::NSWindowCollectionBehavior;
-use tauri_nspanel::{WebviewWindowExt, panel_delegate};
+use tauri::{AppHandle, Emitter, EventTarget, Manager, WebviewWindow};
+use tauri_nspanel::{CollectionBehavior, PanelLevel, StyleMask, WebviewWindowExt, tauri_panel};
 
 const WINDOW_FOCUS_EVENT: &str = "tauri://focus";
 const WINDOW_BLUR_EVENT: &str = "tauri://blur";
-const WINDOW_MOVED_EVENT: &str = "tauri://move";
-const WINDOW_RESIZED_EVENT: &str = "tauri://resize";
+
+tauri_panel! {
+    panel!(NsPanel {
+        config: {
+            is_floating_panel: true,
+            can_become_key_window: true,
+            can_become_main_window: false
+        }
+    })
+
+    panel_event!(NsPanelEventHandler {
+        window_did_become_key(notification: &NSNotification) -> (),
+        window_did_resign_key(notification: &NSNotification) -> (),
+    })
+}
 
 pub fn platform(
     _tauri_app_handle: &AppHandle,
@@ -26,70 +29,39 @@ pub fn platform(
     _check_window: WebviewWindow,
 ) {
     // Convert ns_window to ns_panel
-    let panel = main_window.to_panel().unwrap();
+    let panel = main_window.to_panel::<NsPanel>().unwrap();
+
+    // set level
+    panel.set_level(PanelLevel::Utility.value());
 
     // Do not steal focus from other windows
-    //
-    // Cast is safe
-    panel.set_style_mask(NSNonactivatingPanelMask.0 as i32);
-    // Set its level to NSFloatingWindowLevel to ensure it appears in front of
-    // all normal-level windows
-    //
-    // NOTE: some Chinese input methods use a level between NSDockWindowLevel (20)
-    // and NSMainMenuWindowLevel (24), setting our level above NSDockWindowLevel
-    // would block their window
-    panel.set_floating_panel(true);
+    panel.set_style_mask(StyleMask::empty().nonactivating_panel().into());
 
     // Open the window in the active workspace and full screen
-    #[allow(deprecated)]
-    panel.set_collection_behaviour(
-        NSWindowCollectionBehavior::NSWindowCollectionBehaviorMoveToActiveSpace
-            | NSWindowCollectionBehavior::NSWindowCollectionBehaviorStationary
-            | NSWindowCollectionBehavior::NSWindowCollectionBehaviorFullScreenAuxiliary,
+    panel.set_collection_behavior(
+        CollectionBehavior::new()
+            .stationary()
+            .move_to_active_space()
+            .full_screen_auxiliary()
+            .into(),
     );
 
-    // Define the panel's delegate to listen to panel window events
-    #[allow(deprecated)]
-    let delegate = panel_delegate!(EcoPanelDelegate {
-        window_did_become_key,
-        window_did_resign_key,
-        window_did_resize,
-        window_did_move
-    });
+    let handler = NsPanelEventHandler::new();
 
-    // Set event listeners for the delegate
-    delegate.set_listener(Box::new(move |delegate_name: String| {
+    let window = main_window.clone();
+    handler.window_did_become_key(move |_| {
         let target = EventTarget::labeled(MAIN_WINDOW_LABEL);
 
-        let window_move_event = || {
-            if let Ok(position) = main_window.outer_position() {
-                let _ = main_window.emit_to(target.clone(), WINDOW_MOVED_EVENT, position);
-            }
-        };
+        let _ = window.emit_to(target, WINDOW_FOCUS_EVENT, true);
+    });
 
-        match delegate_name.as_str() {
-            // Called when the window gets keyboard focus
-            "window_did_become_key" => {
-                let _ = main_window.emit_to(target, WINDOW_FOCUS_EVENT, true);
-            }
-            // Called when the window loses keyboard focus
-            "window_did_resign_key" => {
-                let _ = main_window.emit_to(target, WINDOW_BLUR_EVENT, true);
-            }
-            // Called when the window size changes
-            "window_did_resize" => {
-                window_move_event();
+    let window = main_window.clone();
+    handler.window_did_resign_key(move |_| {
+        let target = EventTarget::labeled(MAIN_WINDOW_LABEL);
 
-                if let Ok(size) = main_window.inner_size() {
-                    let _ = main_window.emit_to(target, WINDOW_RESIZED_EVENT, size);
-                }
-            }
-            // Called when the window position changes
-            "window_did_move" => window_move_event(),
-            _ => (),
-        }
-    }));
+        let _ = window.emit_to(target, WINDOW_BLUR_EVENT, true);
+    });
 
     // Set the delegate object for the window to handle window events
-    panel.set_delegate(delegate);
+    panel.set_event_handler(Some(handler.as_ref()));
 }
