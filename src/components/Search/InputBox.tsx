81dc5135e35dd82c8daeb6843be34498d98c56f0
rain9 <15911122312@163.com>
2025-12-09 21:25:25 +0800

    style: adjust styles

--- src/components/Search/InputBox.tsx
+++ src/components/Search/InputBox.tsx
@@ -315,7 +315,7 @@ export default function ChatInput({
         {visibleSearchBar() && (
           <div
             className={clsx(
-              "relative w-full p-2 bg-[#ededed] dark:bg-[#202126]",
+              "relative w-full p-[7px] bg-[#ededed] dark:bg-[#202126]",
               {
                 "flex items-center gap-2": lineCount === 1,
               }
