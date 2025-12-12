3255b4d87d350c2e1d3e14f388767c0b593d7b42
rain9 <15911122312@163.com>
2025-12-12 18:30:58 +0800

    chore: up

--- tsup.config.ts
+++ tsup.config.ts
@@ -110,7 +110,7 @@ export default defineConfig({
 
     const packageJson = {
       name: "@infinilabs/search-chat",
-      version: "1.2.54-beat.7",
+      version: "1.2.54-beat.9",
       main: "index.js",
       module: "index.js",
       type: "module",
