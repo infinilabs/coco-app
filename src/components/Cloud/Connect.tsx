1db871df13312f0d41e3d37b9929d53268eb506b
ayang <473033518@qq.com>
2025-12-19 15:45:49 +0800

    refactor: disable input auto-correction

--- src/components/Cloud/Connect.tsx
+++ src/components/Cloud/Connect.tsx
@@ -63,6 +63,7 @@ export function Connect({ setIsConnect, onAddServer }: ConnectServiceProps) {
               type="text"
               id="endpoint"
               value={endpointLink}
+              autoCorrect="off"
               placeholder={t("cloud.connect.serverPlaceholder")}
               onChange={onChangeEndpoint}
               className="text-[#101010] dark:text-white flex-1 px-3 py-2 border border-gray-300 dark:border-gray-600 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent dark:bg-gray-800"
