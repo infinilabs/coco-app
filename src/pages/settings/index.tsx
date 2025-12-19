8a2898b0b961e065b8823e5263587090c69fb764
ayang <473033518@qq.com>
2025-12-19 12:08:50 +0800

    refactor: update

--- src/pages/settings/index.tsx
+++ src/pages/settings/index.tsx
@@ -87,26 +87,10 @@ function SettingsPage() {
       selectedTab === "extensions" ? "hidden" : "auto";
   }, [selectedTab]);
 
-  const [date, setDate] = useState<Date | undefined>(new Date())
+  const [date, setDate] = useState<Date | undefined>(new Date());
 
   return (
     <>
-      <div className="flex flex-col gap-4 p-10">
-        <Calendar
-          mode="single"
-          selected={date}
-          onSelect={setDate}
-          className="rounded-md border shadow-sm"
-          captionLayout="dropdown"
-        />
-
-        <Calendar
-          mode="range"
-          numberOfMonths={2}
-          className="rounded-md border border-border shadow-sm"
-        />
-      </div>
-
       <div className="min-h-screen pb-8 bg-background text-foreground">
         <div className="max-w-6xl mx-auto p-4">
           <Tabs
