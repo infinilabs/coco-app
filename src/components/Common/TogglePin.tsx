c7f86a035cf140cbf28bb587c8b8dac1ccd4bad0
ayang <473033518@qq.com>
2025-12-31 13:55:45 +0800

    refactor: remove the pin button in the standalone window

--- src/components/Common/TogglePin.tsx
+++ src/components/Common/TogglePin.tsx
@@ -2,9 +2,12 @@ import { useAppStore } from "@/stores/appStore";
 import { useShortcutsStore } from "@/stores/shortcutsStore";
 import clsx from "clsx";
 import VisibleKey from "./VisibleKey";
-import { FC, HTMLAttributes } from "react";
+import { FC, HTMLAttributes, useState } from "react";
 import PinOffIcon from "@/icons/PinOff";
 import PinIcon from "@/icons/Pin";
+import platformAdapter from "@/utils/platformAdapter";
+import { useMount } from "ahooks";
+import { MAIN_WINDOW_LABEL } from "@/constants";
 
 interface TogglePinProps extends HTMLAttributes<HTMLButtonElement> {
   setIsPinnedWeb?: (value: boolean) => void;
@@ -14,6 +17,13 @@ const TogglePin: FC<TogglePinProps> = (props) => {
   const { className, setIsPinnedWeb } = props;
   const { isPinned, setIsPinned } = useAppStore();
   const { fixedWindow } = useShortcutsStore();
+  const [windowLabel, setWindowLabel] = useState<string>();
+
+  useMount(async () => {
+    const label = await platformAdapter.getCurrentWindowLabel();
+
+    setWindowLabel(label);
+  });
 
   const togglePin = async () => {
     const { isTauri, isPinned } = useAppStore.getState();
@@ -34,16 +44,18 @@ const TogglePin: FC<TogglePinProps> = (props) => {
   };
 
   return (
-    <button
-      onClick={togglePin}
-      className={clsx(className, {
-        "text-blue-500": isPinned,
-      })}
-    >
-      <VisibleKey shortcut={fixedWindow} onKeyPress={togglePin}>
-        {isPinned ? <PinIcon /> : <PinOffIcon />}
-      </VisibleKey>
-    </button>
+    windowLabel === MAIN_WINDOW_LABEL && (
+      <button
+        onClick={togglePin}
+        className={clsx(className, {
+          "text-blue-500": isPinned,
+        })}
+      >
+        <VisibleKey shortcut={fixedWindow} onKeyPress={togglePin}>
+          {isPinned ? <PinIcon /> : <PinOffIcon />}
+        </VisibleKey>
+      </button>
+    )
   );
 };
 
